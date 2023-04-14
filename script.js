bce0b8c42d372c5e4c27ef65d5b518f30b13d538
tapir24 <arkananta.budicahyono@gmail.com>
2023-04-14 20:30:43 +0700

    clock java script

---
+++ script.js
@@ -0,0 +1,16 @@
+// var number=10;
+// var number2=115.2;
+// console.log(number);
+// console.log(number+number2);
+// console.log(number-number2);
+// console.log(number+=number2);
+// console.log(number2-=2);
+// console.log(number);
+// console.log(number2);
+// console.log(number*number2);
+// console.log(number/number2);
+// console.log(number<number2);
+// console.log(1>2&&2>1);
+// console.log((1>2)||(2>1));
+// console.log(!(1>2))
+// console.log(1==="1")
