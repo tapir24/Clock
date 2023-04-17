984c816bd8008dcde1b46fa6eefebe0a4b77ac7f
tapir24 <arkananta.budicahyono@gmail.com>
2023-04-17 12:56:40 +0700

    final digtal clock

--- script.js
+++ script.js
@@ -14,3 +14,16 @@
 // console.log((1>2)||(2>1));
 // console.log(!(1>2))
 // console.log(1==="1")
+function showtime(){
+    var date=new Date();
+    var hour=date.getHours();
+    var min=date.getMinutes();
+    var sec=date.getSeconds();
+    sec=(sec<10)?"0"+sec:sec
+    min=(min<10)?"0"+min:min
+    hour=(hour<10)?"0"+hour:hour
+    document.getElementById("myclockdisplay").innerText=hour+"-"+min+"-"+sec;
+    console.log(hour);
+    setTimeout(showtime,1000);
+}
+showtime();
