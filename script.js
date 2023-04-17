0ff97a6c3b5ef0911d325d6b57c83f61455f1c0b
tapir24 <arkananta.budicahyono@gmail.com>
2023-04-17 13:01:50 +0700

    12 hour version

--- script.js
+++ script.js
@@ -19,10 +19,18 @@ function showtime(){
     var hour=date.getHours();
     var min=date.getMinutes();
     var sec=date.getSeconds();
+    var ses="am";
+    if(hour==0){
+        hour=12;
+    }
+    if(hour>12){
+        hour=hour-12;
+        ses="pm";
+    }
     sec=(sec<10)?"0"+sec:sec
     min=(min<10)?"0"+min:min
     hour=(hour<10)?"0"+hour:hour
-    document.getElementById("myclockdisplay").innerText=hour+"-"+min+"-"+sec;
+    document.getElementById("myclockdisplay").innerText=hour+"-"+min+"-"+sec+" "+ses;
     console.log(hour);
     setTimeout(showtime,1000);
 }
